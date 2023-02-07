b41ae93f12134ae0e446139a9dba3505ea33c0fd
unknown <acostacamilo483@gmail.com>
2023-02-06 21:38:53 -0500

    se sesube repositrio

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { Route, Routes } from "react-router-dom";
 import React from "react";
 import Main from "./componentes/Main";
 import stilo from "./componentes/imagenes/hoja-stilo/stilo.css"
+import Baner from "./componentes/Baner";
 
 function App() {
   return (
    < React.Fragment>
     <Routes>
     <Route path="/" element={ <Main/>}/>
-
+    <Route path="/nosotros" element={ <Baner/>}/>
     </Routes>
 
    </React.Fragment>
